029dc5c60d45099fbd239fd360dd181c7ccab845
junjl1 <1106513621@qq.com>
2021-08-08 21:29:36 +0800

    first blog commit

---
+++ .vuepress/config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "title": "Junjl",
+  "description": "求其记记",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "modeTheme": "dark",
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+    ],
+    "sidebar": "auto",
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Junjl",
+    "authorAvatar": "/avatar.png",
+    "record": "",
+    "startYear": "2021"
+  },
+  "markdown": {
+    "lineNumbers": true
+  },
+  "codeTheme": "default"
+}
